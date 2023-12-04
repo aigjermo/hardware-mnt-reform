e98e9866cbb33a5e7ff6c421a7ce0f228ddc5560
Lykso <hardware-mnt-reform@lyk.so>
2023-12-04 12:53:06 -0800

    Bump kernel to 6.1.64

--- kernel/linux-6.1.nix
+++ kernel/linux-6.1.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.1.62";
+  version = "6.1.64";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   majorVersion = lib.head (splitVersion version);
@@ -14,6 +14,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v${majorVersion}.x/linux-${version}.tar.xz";
-    sha256 = "sha256-uf1hb6zWvs/O74i5vnGNDxZiXKs/6B0ROEgCpwkehew="; # 6.1.62
+    sha256 = "sha256-Yp2qOPPqZ/KWEL+9U/nzj0aDTTZURR6UdBAEkMZtx+c=";
   };
 } // (args.argsOverride or {}))
