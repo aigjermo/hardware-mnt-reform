cd299c14e8231ac179d4d85676f1c8cf60cafdc8
Lykso <hardware-mnt-reform@lyk.so>
2024-01-06 12:17:30 -0800

    Bump kernel to 6.1.71

--- kernel/linux-lts.nix
+++ kernel/linux-lts.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "6.1.70";
+  version = "6.1.71";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   majorVersion = lib.head (splitVersion version);
@@ -14,6 +14,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v${majorVersion}.x/linux-${version}.tar.xz";
-    sha256 = "sha256-7RNlJmRWwHaWp0mVga7F2FHKIpb09vkPI9GJ6lpWr+8=";
+    sha256 = "sha256-Lfd03VP5/9Tlfr+ATPWXcJKV32owT+Jh0lIgoTS38EE=";
   };
 } // (args.argsOverride or {}))
