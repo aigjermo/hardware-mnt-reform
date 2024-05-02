798e17ba1e703204845974df8a71d13813d067c4
Lykso <hardware-mnt-reform@lyk.so>
2024-05-02 09:35:23 -0700

    Bump IMX8MQ kernel to 6.1.90

--- imx8mq/kernel.nix
+++ imx8mq/kernel.nix
@@ -1,6 +1,6 @@
 { stdenv, lib, config, pkgs, buildLinux, fetchurl, fetchgit, linux, kernelPatches, ... }@args:
 (pkgs.callPackage ../common/kernel {
   inherit kernelPatches;
-  version = "6.1.89";
-  hash = "sha256-Erq44JJhjR1O6vQgHm5wBUyUiWGYlWvYT/DpCLAmRxk=";
+  version = "6.1.90";
+  hash = "sha256-g6PXLnZPztosH8aKTqa5ElOijaVqaIorYXdrDRl4jh0=";
 }).forSoc "imx8mq-mnt-reform2" "0x40480000"
