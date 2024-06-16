1f86a28a0df099164e54670e15cefd23ccf82cfd
Anders Iver Gjermo <code@ai.gjermo.com>
2024-06-17 00:40:00 +0200

    use default kernel on imx8mq

--- imx8mq/kernel.nix
+++ imx8mq/kernel.nix
@@ -1,6 +1,6 @@
 { stdenv, lib, config, pkgs, buildLinux, fetchurl, fetchgit, linux, kernelPatches, ... }@args:
 (pkgs.callPackage ../common/kernel {
   inherit kernelPatches;
-  version = "6.1.93";
-  hash = "sha256-3zGvLvWSPWH63Wi/2ZH1Dy5CqROJXrSwMhTuePhyC88=";
+  #version = "1.2.3";
+  #hash = lib.fakeHash;
 }).forSoc "imx8mq-mnt-reform2" "0x40480000"
